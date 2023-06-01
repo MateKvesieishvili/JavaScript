ca41287f58b8b4008e99334558f933b5eb947c30
Mate Kvesieishvili <129661796+MateKvesieishvili@users.noreply.github.com>
2023-06-01 18:39:38 +0400

    Homework6

---
+++ Workshop/script.js
@@ -0,0 +1,372 @@
+
+const data = [
+  {
+    id: 1,
+    firstname: "Molly",
+    lastname: "Begbie",
+    email: "mbegbie0@parallels.com",
+    gender: "Male",
+    age: 12,
+    job: "Tax Accountant",
+  },
+  {
+    id: 2,
+    firstname: "Mayer",
+    lastname: "Kuhnke",
+    email: "mkuhnke1@myspace.com",
+    gender: "Female",
+    age: 39,
+    job: "Legal Assistant",
+  },
+  {
+    id: 3,
+    firstname: "Susy",
+    lastname: "Kyneton",
+    email: "skyneton2@buzzfeed.com",
+    gender: "Male",
+    age: 36,
+    job: "Director of Sales",
+  },
+  {
+    id: 4,
+    firstname: "Raquela",
+    lastname: "Littlefield",
+    email: "rlittlefield3@google.co.uk",
+    gender: "Male",
+    age: 69,
+    job: "Business Systems Development Analyst",
+  },
+  {
+    id: 5,
+    firstname: "Robinetta",
+    lastname: "Loughney",
+    email: "rloughney4@typepad.com",
+    gender: "Female",
+    age: 40,
+    job: "Financial Advisor",
+  },
+  {
+    id: 6,
+    firstname: "Orelle",
+    lastname: "Domanski",
+    email: "odomanski5@angelfire.com",
+    gender: "Male",
+    age: 32,
+    job: "VP Accounting",
+  },
+  {
+    id: 7,
+    firstname: "Brandi",
+    lastname: "Haliburton",
+    email: "bhaliburton6@baidu.com",
+    gender: "Male",
+    age: 57,
+    job: "Engineer IV",
+  },
+  {
+    id: 8,
+    firstname: "Ario",
+    lastname: "Cormack",
+    email: "acormack7@sfgate.com",
+    gender: "Male",
+    age: 43,
+    job: "Speech Pathologist",
+  },
+  {
+    id: 9,
+    firstname: "Kirsti",
+    lastname: "Kimmerling",
+    email: "kkimmerling8@statcounter.com",
+    gender: "Female",
+    age: 27,
+    job: "Design Engineer",
+  },
+  {
+    id: 10,
+    firstname: "Orton",
+    lastname: "Renachowski",
+    email: "orenachowski9@amazon.co.uk",
+    gender: "Male",
+    age: 55,
+    job: "Technical Writer",
+  },
+  {
+    id: 11,
+    firstname: "Myca",
+    lastname: "Blinkhorn",
+    email: "mblinkhorna@geocities.jp",
+    gender: "Male",
+    age: 42,
+    job: "Recruiter",
+  },
+  {
+    id: 12,
+    firstname: "Willdon",
+    lastname: "Jennaway",
+    email: "wjennawayb@wunderground.com",
+    gender: "Male",
+    age: 35,
+    job: "Design Engineer",
+  },
+  {
+    id: 13,
+    firstname: "Clarinda",
+    lastname: "Cockley",
+    email: "ccockleyc@plala.or.jp",
+    gender: "Male",
+    age: 72,
+    job: "Design Engineer",
+  },
+  {
+    id: 14,
+    firstname: "Alister",
+    lastname: "Grayham",
+    email: "agrayhamd@technorati.com",
+    gender: "Female",
+    age: 24,
+    job: "Speech Pathologist",
+  },
+  {
+    id: 15,
+    firstname: "Gloria",
+    lastname: "Nell",
+    email: "gnelle@parallels.com",
+    gender: "Female",
+    age: 58,
+    job: "Senior Cost Accountant",
+  },
+  {
+    id: 16,
+    firstname: "Lanette",
+    lastname: "Jendas",
+    email: "ljendasf@cdc.gov",
+    gender: "Female",
+    age: 49,
+    job: "Quality Control Specialist",
+  },
+  {
+    id: 17,
+    firstname: "Emmalee",
+    lastname: "McLenahan",
+    email: "emclenahang@samsung.com",
+    gender: "Female",
+    age: 54,
+    job: "Research Associate",
+  },
+  {
+    id: 18,
+    firstname: "Cory",
+    lastname: "Mayhew",
+    email: "cmayhewh@nyu.edu",
+    gender: "Male",
+    age: 71,
+    job: "VP Product Management",
+  },
+  {
+    id: 19,
+    firstname: "Neil",
+    lastname: "Marchington",
+    email: "nmarchingtoni@163.com",
+    gender: "Male",
+    age: 21,
+    job: "Senior Cost Accountant",
+  },
+  {
+    id: 20,
+    firstname: "Sidney",
+    lastname: "Wiltshire",
+    email: "swiltshirej@timesonline.co.uk",
+    gender: "Male",
+    age: 52,
+    job: "Tax Accountant",
+  },
+  {
+    id: 21,
+    firstname: "Kitti",
+    lastname: "Syfax",
+    email: "ksyfaxk@wunderground.com",
+    gender: "Female",
+    age: 61,
+    job: "Internal Auditor",
+  },
+  {
+    id: 22,
+    firstname: "Anatole",
+    lastname: "Goodfield",
+    email: "agoodfieldl@desdev.cn",
+    gender: "Male",
+    age: 56,
+    job: "Mechanical Systems Engineer",
+  },
+  {
+    id: 23,
+    firstname: "Kennith",
+    lastname: "Winridge",
+    email: "kwinridgem@nba.com",
+    gender: "Female",
+    age: 48,
+    job: "Safety Technician III",
+  },
+  {
+    id: 24,
+    firstname: "Gard",
+    lastname: "Klosa",
+    email: "gklosan@amazon.co.uk",
+    gender: "Male",
+    age: 56,
+    job: "GIS Technical Architect",
+  },
+  {
+    id: 25,
+    firstname: "Bethanne",
+    lastname: "Gyrgorcewicx",
+    email: "bgyrgorcewicxo@addtoany.com",
+    gender: "Female",
+    age: 53,
+    job: "Teacher",
+  },
+  {
+    id: 26,
+    firstname: "Yancey",
+    lastname: "Bilson",
+    email: "ybilsonp@washingtonpost.com",
+    gender: "Female",
+    age: 22,
+    job: "Financial Advisor",
+  },
+  {
+    id: 27,
+    firstname: "Gerrie",
+    lastname: "Borgesio",
+    email: "gborgesioq@uol.com.br",
+    gender: "Male",
+    age: 61,
+    job: "Food Chemist",
+  },
+  {
+    id: 28,
+    firstname: "Corny",
+    lastname: "Garnham",
+    email: "cgarnhamr@ed.gov",
+    gender: "Male",
+    age: 79,
+    job: "Assistant Professor",
+  },
+  {
+    id: 29,
+    firstname: "Hervey",
+    lastname: "Hackwell",
+    email: "hhackwells@jigsy.com",
+    gender: "Male",
+    age: 67,
+    job: "Web Designer I",
+  },
+  {
+    id: 30,
+    firstname: "Carey",
+    lastname: "Swindall",
+    email: "cswindallt@comsenz.com",
+    gender: "Female",
+    age: 81,
+    job: "Nuclear Power Engineer",
+  },
+];
+
+// problem 1
+// დაწერეთ ფუნქცია რომელიც პარამეტრად მიცემული მაისივიდან დააბრუნებს მომხმარებლების საშუალო ასაკს.
+
+function calculateAverageAge(data){
+    let totalAge = 0
+    for (const person of data) {
+       totalAge += person.age
+    }
+    const averageAge = totalAge / data.length
+    return averageAge
+}
+
+let averageAge = calculateAverageAge(data);
+console.log("Average Age:", averageAge)
+
+// problem 2
+// !დაწერეთ ფუნქცია რომელიც დააბრუნებს მასივს სადაც იქნება მხოლოდ მომხმარებლების Email.
+
+function getEmails(data){
+    const emails = []
+
+    let forUserEmails = data.forEach(user => {
+        emails.push(data.email)
+    });
+
+    return emails
+}
+
+let emails = getEmails(data);
+console.log("Emails:", emails);
+
+// ? აქ რატომღაც რაც არ უნდა მეცადა email-ზე მიწერდა რო undefined იყო
+
+
+// problem 3
+// დაწერეთ ფუნქცია რომელიც დააბრუნებს რამდენი ქალი და რამდენი კაცია გადაცემულ მასივში.
+
+function countGenders(data) {
+    let femaleCount = 0;
+    let maleCount = 0;
+  
+    for (const person of data) {
+      if (person.gender === "Female") {
+        femaleCount++;
+      } else if (person.gender === "Male") {
+        maleCount++;
+      }
+    }
+  
+    console.log("Females:", femaleCount, "Males:" , maleCount)
+  }
+
+countGenders(data)
+
+// problem 4
+// დაწერეთ ფუნქცია რომელსაც პარამეტრად გადავაწოდებთ მასივს და მასივში მოსაძებნ ელემენტს.
+// ფუნქციამ უნდა დააბრუნოს ელემენტის ინდექსი მასივში.
+
+let nameArray = ["Gio", "Nika", "Mate", "Sofo", "Nini"]
+
+function searchElementArray(){
+    
+    nameArray.forEach((element, index) => {
+        console.log(`Index: ${index}, Element: ${element}`);
+      });
+
+}
+
+console.log(searchElementArray())
+
+// problem 5
+
+// დაწერეთ ფუნქცია რომელსაც გადავაწოდებთ 3 პარამეტრს.
+// 2 მასივს და 1 ელემენტს.
+// თუ ეს ელემენტი ორივე მასივში მოიძებნება დავაბრუნებთ სტრინგს სადაც ეწერება
+// რომ ელემენტი ორივე მასივში მოიძებნა.
+// თუ მხოლოდ ერთერთ მასივში იქნება დავაბრუნებთ რომელ მასივში მოიძებნა (მაგ: პირველ მასივში გვაქვს ეს ელემენტი ან მეორე მასივში გვაქვს ეს ელემენტი)
+// თუ არცერთში არ იყო დავაბრუნებთ რომ არცერთ მასივში არ გვაქვს მსგავსი ელემენტი.
+
+let array1 = [2, 4, 8, 10, 24, 46, 90, 54, 12];
+let array2 = [12, 44, 81, 10, 243, 46, 90, 154, 12];
+
+function findElement(array1, array2, element) {
+
+    const foundInArray1 = array1.includes(element)
+    const foundInArray2 = array2.includes(element)
+
+    if(foundInArray1 && foundInArray2){
+        return "ელემენტი ორივე მასივში მოიძებნა"
+    } else if(foundInArray1){
+        return "ელემენტი მხოლოდ პირველ მასივში მოიძებნა"
+    } else if (foundInArray2){
+        return "ელემენტი მხოლოდ მეორ მასივში მოიძებნა"
+    } else{
+        return "ელემენტი არცერთ მასივში მოიძებნა"
+    } 
+}
+

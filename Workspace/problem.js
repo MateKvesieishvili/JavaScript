a304b258504cbafbdee5322a38b10b5aa6be5f73
Mate Kvesieishvili <129661796+MateKvesieishvili@users.noreply.github.com>
2023-06-21 00:27:48 +0400

    Homework 11

--- Workspace/problem.js
+++ Workspace/problem.js
@@ -1,28 +1,139 @@
-
-
-// დავალებების შესასრულებლად გამოიყენეთ ბოლო ლექციაზე ნასწავლი მეთოდები.
-
-// problem 1
-// დაწერეთ ფუნქცია რომელიც პარამეტრად მიცემული მაისივიდან დააბრუნებს მომხმარებლების საშუალო ასაკს.
-// problem 2
-// დაწერეთ ფუნქცია რომელიც პარამეტრად მიღებული მაისივიდან დააბრუნებს ახალ მასივს
-// სადაც იქნება მომხმარებლები რომლებიც არ არიან 20 წელზე მეტის.
-
-// problem 3
-// დაწერეთ ფუნქცია რომელიც დააბრუნებს მასივს სადაც იქნება მხოლოდ მომხმარებლების სახელი და გვარი.
-
-// problem 4
-// დაწერეთ ფუნქცია რომელიც დააბრუნებს რამდენი ქალი და რამდენი კაცია გადაცემულ მასივში.
-
-// problem 5
-// დაწერეთ ფუნქცია რომელიც დააბრუნებს პირველ მომხმარებელს რომელის ასაკიც 40 წელზე მეტია.
-
-// problem6
-// დაწერეთ ფუნქცია რომელსაც გადავცემთ 2 პარამეტრს. მასივს და ელემენტს რომლის წაშლაც გვინდა მასივიდან
-// თუ მოცემული ელემენტი იქნება გადაცემულ მასივში მაშინ დავაბრუნებთ ახალ მასივს სადაც უკვე ამოშლილი იქნება გადაცემული პარამეტრი
-
-// problem7
-// დაწერეთ ფუნქცია რომელიც დაადგენს პარამეტრად გადაცემული სტრინგი არის თუ არა პალინდრომი
-
-// პალინდრომი არის სიტყვა რომელიც მარცხნიდან და მარჯვნიდან ერთნაირად იკითხება
-// მაგალითად:  kayak, deified, rotator, repaper, deed, peep, wow, noon
+//! დავალების შესასრულებლად, გუგლი და AI გამოვიყენე, ძალიან რთული იყო
+const APP = {
+    init() {
+      APP.addListeners();
+    },
+    addListeners() {
+      let form = document.forms.sampleForm;
+      let fullname = document.getElementById("fullname");
+      let email = document.getElementById("email");
+      let pass = document.getElementById("pass");
+      let cell = document.getElementById("cell");
+  
+      fullname.addEventListener("input", APP.testName);
+      email.addEventListener("input", APP.testEmail);
+      pass.addEventListener("input", APP.checkPasswordRequirements);
+      cell.addEventListener("input", APP.formatPhone);
+  
+      email.addEventListener("invalid", APP.fail);
+      pass.addEventListener("invalid", APP.fail);
+      cell.addEventListener("invalid", APP.fail);
+  
+      form.addEventListener("submit", APP.validate);
+    },
+    validate(ev) {
+      ev.preventDefault();
+      let form = ev.target;
+  
+      let fullname = document.getElementById("fullname");
+      let email = document.getElementById("email");
+      let pass = document.getElementById("pass");
+      let cell = document.getElementById("cell");
+  
+      // Check validity for each field
+      let isFullnameValid = fullname.checkValidity();
+      let isEmailValid = email.checkValidity();
+      let isPasswordValid = pass.checkValidity();
+      let isCellValid = cell.checkValidity();
+  
+      if (!isFullnameValid || !isEmailValid || !isPasswordValid || !isCellValid) {
+        // Handle validation errors
+        if (!isFullnameValid) {
+          fullname.setCustomValidity("Full name must be 'John' or 'Jane'.");
+          fullname.reportValidity();
+        }
+  
+        if (!isEmailValid) {
+          let emReg = new RegExp("@gmail.com$", "i");
+          if (emReg.test(email.value) === false) {
+            email.setCustomValidity("Email must be a valid Gmail address.");
+            email.reportValidity();
+          }
+        }
+  
+        if (!isPasswordValid) {
+          pass.setCustomValidity("Password must meet the requirements.");
+          pass.reportValidity();
+        }
+  
+        if (!isCellValid) {
+          cell.setCustomValidity("Cellphone must be a valid Georgian number starting with +995.");
+          cell.reportValidity();
+        }
+      } else {
+        // All fields are valid, submit the form
+        form.submit();
+      }
+    },
+    testName(ev) {
+      let fullname = ev.target;
+      fullname.setCustomValidity(""); // Clear old message
+  
+      let validNames = ["John", "Jane"];
+      if (!validNames.includes(fullname.value)) {
+        fullname.setCustomValidity("Full name must be 'John' or 'Jane'.");
+        fullname.reportValidity();
+      }
+    },
+    testEmail(ev) {
+      let email = ev.target;
+      email.setCustomValidity(""); // Clear old message
+  
+      let emReg = new RegExp("@gmail.com$", "i");
+      if (!emReg.test(email.value)) {
+        email.setCustomValidity("Email must be a valid Gmail address.");
+        email.reportValidity();
+      }
+    },
+    formatPhone(ev) {
+      let cell = ev.target;
+      cell.setCustomValidity(""); // Clear old message
+  
+      let geReg = new RegExp("^\\+995[\\d]{9}$");
+      if (!geReg.test(cell.value)) {
+        cell.setCustomValidity("Cellphone must be a valid Georgian number starting with +995.");
+        cell.reportValidity();
+      }
+    },
+    checkPasswordRequirements(input) {
+      let pass = input.target;
+      pass.setCustomValidity(""); // Clear old message
+  
+      let requirements = [
+        /[A-Z]/, // Uppercase
+        /[a-z]/, // Lowercase
+        /[0-9]/, // Numeric
+        /^.{10,}$/ // Length >= 10
+      ];
+  
+      let isValid = requirements.every((regex) => regex.test(pass.value));
+      if (!isValid) {
+        pass.setCustomValidity("Password must meet the requirements.");
+        pass.reportValidity();
+      }
+    },
+    fail(ev) {
+      let field = ev.target;
+      field.setCustomValidity(""); // Clear old message
+  
+      switch (field.id) {
+        case "fullname":
+          field.setCustomValidity("Full name must be 'John' or 'Jane'.");
+          break;
+        case "email":
+          field.setCustomValidity("Email must be a valid Gmail address.");
+          break;
+        case "pass":
+          field.setCustomValidity("Password must meet the requirements.");
+          break;
+        case "cell":
+          field.setCustomValidity("Cellphone must be a valid Georgian number starting with +995.");
+          break;
+      }
+  
+      field.reportValidity();
+    },
+  };
+  
+  document.addEventListener("DOMContentLoaded", APP.init);
+  
--- Workspace/script.js
+++ Workspace/script.js
@@ -1,357 +1,104 @@
+problem.js
 
+/****
+ * References
+ * https://developer.mozilla.org/en-US/docs/Web/API/Constraint_validation
+ * https://developer.mozilla.org/en-US/docs/Web/API/ValidityState
+ * https://regexr.com/ - Tool by Grant Skinner for testing Regular Expressions
+ *
+ */
 
-const data = [
-    {
-      id: 1,
-      firstname: "Molly",
-      lastname: "Begbie",
-      email: "mbegbie0@parallels.com",
-      gender: "Non-binary",
-      age: 12,
-      job: "Tax Accountant",
-    },
-    {
-      id: 2,
-      firstname: "Mayer",
-      lastname: "Kuhnke",
-      email: "mkuhnke1@myspace.com",
-      gender: "Agender",
-      age: 39,
-      job: "Legal Assistant",
-    },
-    {
-      id: 3,
-      firstname: "Susy",
-      lastname: "Kyneton",
-      email: "skyneton2@buzzfeed.com",
-      gender: "Male",
-      age: 36,
-      job: "Director of Sales",
-    },
-    {
-      id: 4,
-      firstname: "Raquela",
-      lastname: "Littlefield",
-      email: "rlittlefield3@google.co.uk",
-      gender: "Genderqueer",
-      age: 69,
-      job: "Business Systems Development Analyst",
-    },
-    {
-      id: 5,
-      firstname: "Robinetta",
-      lastname: "Loughney",
-      email: "rloughney4@typepad.com",
-      gender: "Female",
-      age: 40,
-      job: "Financial Advisor",
-    },
-    {
-      id: 6,
-      firstname: "Orelle",
-      lastname: "Domanski",
-      email: "odomanski5@angelfire.com",
-      gender: "Polygender",
-      age: 32,
-      job: "VP Accounting",
-    },
-    {
-      id: 7,
-      firstname: "Brandi",
-      lastname: "Haliburton",
-      email: "bhaliburton6@baidu.com",
-      gender: "Bigender",
-      age: 57,
-      job: "Engineer IV",
-    },
-    {
-      id: 8,
-      firstname: "Ario",
-      lastname: "Cormack",
-      email: "acormack7@sfgate.com",
-      gender: "Bigender",
-      age: 43,
-      job: "Speech Pathologist",
-    },
-    {
-      id: 9,
-      firstname: "Kirsti",
-      lastname: "Kimmerling",
-      email: "kkimmerling8@statcounter.com",
-      gender: "Female",
-      age: 27,
-      job: "Design Engineer",
-    },
-    {
-      id: 10,
-      firstname: "Orton",
-      lastname: "Renachowski",
-      email: "orenachowski9@amazon.co.uk",
-      gender: "Genderfluid",
-      age: 55,
-      job: "Technical Writer",
-    },
-    {
-      id: 11,
-      firstname: "Myca",
-      lastname: "Blinkhorn",
-      email: "mblinkhorna@geocities.jp",
-      gender: "Bigender",
-      age: 42,
-      job: "Recruiter",
-    },
-    {
-      id: 12,
-      firstname: "Willdon",
-      lastname: "Jennaway",
-      email: "wjennawayb@wunderground.com",
-      gender: "Non-binary",
-      age: 35,
-      job: "Design Engineer",
-    },
-    {
-      id: 13,
-      firstname: "Clarinda",
-      lastname: "Cockley",
-      email: "ccockleyc@plala.or.jp",
-      gender: "Genderqueer",
-      age: 72,
-      job: "Design Engineer",
-    },
-    {
-      id: 14,
-      firstname: "Alister",
-      lastname: "Grayham",
-      email: "agrayhamd@technorati.com",
-      gender: "Genderfluid",
-      age: 24,
-      job: "Speech Pathologist",
-    },
-    {
-      id: 15,
-      firstname: "Gloria",
-      lastname: "Nell",
-      email: "gnelle@parallels.com",
-      gender: "Non-binary",
-      age: 58,
-      job: "Senior Cost Accountant",
-    },
-    {
-      id: 16,
-      firstname: "Lanette",
-      lastname: "Jendas",
-      email: "ljendasf@cdc.gov",
-      gender: "Polygender",
-      age: 49,
-      job: "Quality Control Specialist",
-    },
-    {
-      id: 17,
-      firstname: "Emmalee",
-      lastname: "McLenahan",
-      email: "emclenahang@samsung.com",
-      gender: "Female",
-      age: 54,
-      job: "Research Associate",
-    },
-    {
-      id: 18,
-      firstname: "Cory",
-      lastname: "Mayhew",
-      email: "cmayhewh@nyu.edu",
-      gender: "Agender",
-      age: 71,
-      job: "VP Product Management",
-    },
-    {
-      id: 19,
-      firstname: "Neil",
-      lastname: "Marchington",
-      email: "nmarchingtoni@163.com",
-      gender: "Female",
-      age: 21,
-      job: "Senior Cost Accountant",
-    },
-    {
-      id: 20,
-      firstname: "Sidney",
-      lastname: "Wiltshire",
-      email: "swiltshirej@timesonline.co.uk",
-      gender: "Genderfluid",
-      age: 52,
-      job: "Tax Accountant",
-    },
-    {
-      id: 21,
-      firstname: "Kitti",
-      lastname: "Syfax",
-      email: "ksyfaxk@wunderground.com",
-      gender: "Female",
-      age: 61,
-      job: "Internal Auditor",
-    },
-    {
-      id: 22,
-      firstname: "Anatole",
-      lastname: "Goodfield",
-      email: "agoodfieldl@desdev.cn",
-      gender: "Genderqueer",
-      age: 56,
-      job: "Mechanical Systems Engineer",
-    },
-    {
-      id: 23,
-      firstname: "Kennith",
-      lastname: "Winridge",
-      email: "kwinridgem@nba.com",
-      gender: "Female",
-      age: 48,
-      job: "Safety Technician III",
-    },
-    {
-      id: 24,
-      firstname: "Gard",
-      lastname: "Klosa",
-      email: "gklosan@amazon.co.uk",
-      gender: "Agender",
-      age: 56,
-      job: "GIS Technical Architect",
-    },
-    {
-      id: 25,
-      firstname: "Bethanne",
-      lastname: "Gyrgorcewicx",
-      email: "bgyrgorcewicxo@addtoany.com",
-      gender: "Agender",
-      age: 53,
-      job: "Teacher",
-    },
-    {
-      id: 26,
-      firstname: "Yancey",
-      lastname: "Bilson",
-      email: "ybilsonp@washingtonpost.com",
-      gender: "Female",
-      age: 22,
-      job: "Financial Advisor",
-    },
-    {
-      id: 27,
-      firstname: "Gerrie",
-      lastname: "Borgesio",
-      email: "gborgesioq@uol.com.br",
-      gender: "Polygender",
-      age: 61,
-      job: "Food Chemist",
-    },
-    {
-      id: 28,
-      firstname: "Corny",
-      lastname: "Garnham",
-      email: "cgarnhamr@ed.gov",
-      gender: "Bigender",
-      age: 79,
-      job: "Assistant Professor",
-    },
-    {
-      id: 29,
-      firstname: "Hervey",
-      lastname: "Hackwell",
-      email: "hhackwells@jigsy.com",
-      gender: "Bigender",
-      age: 67,
-      job: "Web Designer I",
-    },
-    {
-      id: 30,
-      firstname: "Carey",
-      lastname: "Swindall",
-      email: "cswindallt@comsenz.com",
-      gender: "Female",
-      age: 81,
-      job: "Nuclear Power Engineer",
-    },
-  ];
+// დაამატეთ ვალიდაციები ინფუთებს რომლებსაც არ აქვთ,
+//  სახელზე დამატეთ რომ ჩაწერილი იყოს მხოლოდ john ან jane ქეის სენსითივი არ უნდა იყოს.
+// ტელეფონზე დაამატეთ რომ იყოს ქართული ნომერი, ანუ იწყებოდეს +995 და თვითონ ნომერი არ იქყოს 9 სიმბოლოზე მეტი მაგალითად (+995599909090)
+// პაროლზე თვითონ ფუნქციაში მიწერია როგორი უნდა იყოს პაროლი.
 
-//   Problem 1
+const APP = {
+  init() {
+    APP.addListeners();
+  },
+  addListeners() {
+    let form = document.sampleForm;
+    let email = document.getElementById("email");
+    email.addEventListener("change", APP.testEmail);
+    //what to do if something went wrong during validation
+    email.addEventListener("invalid", APP.fail);
 
-function calculateAverageAge(data) {
-    let totalAge = 0;
-    for (const person of data) {
-      totalAge += person.age;
-    }
-    const averageAge = totalAge / data.length;
-    return averageAge;
-  }
+    //when the form gets submitted
+    form.addEventListener("submit", APP.validate);
+  },
+  validate(ev) {
+    ev.preventDefault();
+    let form = ev.target;
 
-let averageAge = calculateAverageAge(data);
-console.log("Average Age:", averageAge);
+    let email = document.getElementById("email");
+    console.log("willValidate", email.willValidate);
+    //run validation on the whole form when submitting...
 
+    // form controls have the following
+    // invalid event
+    console.log(email.validity);
+    // validity readonly prop - a ValidityState object
+    // ValidityState object props: (Boolean values)
+    // badInput, customError, patternMismatch, rangeOverflow, rangeUnderflow
+    // stepMismatch, tooLong, tooShort, typeMismatch, valid, valueMissing
+    //
+    // willValidate readonly prop - boolean
 
-// Problem 2
+    // validationMessage - readonly prop from browser validation
+    //                    or setCustomValidity( ) method
 
-function filterPersonsUnder20(data) {
-    let personsUnder20 = [];
-    for (const person of data) {
-      if (person.age < 20) {
-        personsUnder20.push(person);
+    // checkValidity() checks element, returns boolean,
+    //                fires the invalid event
+
+    // reportValidity() checks AND reports result
+    //                  this shows the browser tooltip with warning
+    //                  can be called at any point to show message
+
+    // setCustomValidity(msg) if called with non-empty string it
+    //                    will change the value of validity.valid
+    //                    to false and validity.customError to true
+  },
+  testName(ev) {},
+  testEmail(ev) {
+    let email = ev.target;
+    console.log("willValidate | ", email.willValidate);
+    console.log("validity object ", email.validity);
+
+    email.setCustomValidity(""); //clear old message
+    //built-in test for error based on type, pattern, and other attrs
+    let currently = email.checkValidity();
+    console.log("currently", currently);
+    console.log("validation message | ", email.validationMessage);
+
+    if (currently) {
+      let emReg = new RegExp("@gmail.com$", "i");
+      if (emReg.test(email.value) === false) {
+        //not a gmail address
+        console.log("NOT a gmail address");
+        email.setCustomValidity("NOT a gmail address.");
+        console.log(email.validity);
+        email.reportValidity(); //show the custom message, trigger invalid event
       }
     }
-    return personsUnder20;
-  }
-
-let personsUnder20 = filterPersonsUnder20(data);
-console.log("Persons Younger Than 20:", personsUnder20);
-
-// Problem 3
-function getNamesAndSurnames(data) {
-    let namesAndSurnames = data.map(person => `${person.firstname} ${person.lastname}`);
-    return namesAndSurnames;
-}
-
-let namesAndSurnames = getNamesAndSurnames(data);
-console.log("Names and Surnames:", namesAndSurnames);
-
-// Problem 4
-function countGenders(data) {
-    let femaleCount = 0;
-    let maleCount = 0;
-  
-    for (const person of data) {
-      if (person.gender === "Female") {
-        femaleCount++;
-      } else if (person.gender === "Male") {
-        maleCount++;
-      }
+  },
+  formatPhone(ev) {},
+  checkPasswordRequirements(input) {
+    //check password requirements as user types
+    // uppercase, lowercase, numeric, length >= 10
+    // Allowed: [! @ # $ % ^ & * ( ) . , ? ; : ~]
+  },
+  fail(ev) {
+    let field = ev.target;
+    // the invalid event fired
+    console.log("INVALID");
+    //standard display change for any element
+    switch (field.id) {
+      case "email":
+        let span = field.parentElement.querySelector(".errMessage");
+        span.textContent = "Must be a valid Gmail address.";
+        break;
     }
-  
-    return {
-      female: femaleCount,
-      male: maleCount
-    };
-  }
-//   Problem5
-function getFirstUserOver40(data) {
-    let userOver40 = data.find(person => person.age > 40);
-    return userOver40;
-  }
-
-  let firstUserOver40 = getFirstUserOver40(data);
-console.log("First User Over 40:", firstUserOver40);
-// Problem 6
-let myArray = [1, 2, 3, 4, 5];
-let elementToRemove = 3;
-let newArray = removeElementFromArray(myArray, elementToRemove);
-
-console.log("Original Array:", myArray);
-console.log("New Array:", newArray);
-// Problem 7
-function isPalindrome(str) {
-    const reversed = str.split('').reverse().join('');
-    return str === reversed;
-  }
-  
-  const input = "racecar";
-  const result = isPalindrome(input);
-  
-  console.log(`Is "${input}" a palindrome? ${result}`);
+  },
+};
+document.addEventListener("DOMContentLoaded", APP.init);
